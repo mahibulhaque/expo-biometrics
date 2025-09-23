74b7a6f8a27b6fa8f55c98ad5f3058b538c58a96
MahibulHaque <mahibulhaque2000@gmail.com>
2025-09-24 02:17:53 +0600

    chore: remove web view files

--- src/ExpoBiometricsView.tsx
+++ src/ExpoBiometricsView.tsx
@@ -1,10 +1,10 @@
-import { requireNativeView } from 'expo';
-import * as React from 'react';
+import { requireNativeView } from "expo";
+import * as React from "react";
 
-import { ExpoBiometricsViewProps } from './ExpoBiometrics.types';
+import { ExpoBiometricsViewProps } from "./ExpoBiometrics.types";
 
 const NativeView: React.ComponentType<ExpoBiometricsViewProps> =
-  requireNativeView('ExpoBiometrics');
+  requireNativeView("ExpoBiometrics");
 
 export default function ExpoBiometricsView(props: ExpoBiometricsViewProps) {
   return <NativeView {...props} />;
